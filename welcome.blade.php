62ca62bf0a2933ccaa5aa7a1e39a8ea521b25bf9
Sangintan04 <91972469+Sangintan04@users.noreply.github.com>
2021-11-24 23:23:35 +0700

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,11 @@
+<div class="content">
+    <div class="title m=b-md">
+        5026201051 <br>
+        sang intan risqi 
+</div>
+
+<div class="links">
+    <a href="">Tugas 1</a>
+    <a href="">Tugas 2</a>
+    <a href="">Tugas 3</a>
+    <a href="">praktikum 1</a>
